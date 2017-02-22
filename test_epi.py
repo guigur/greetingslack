662c2bf1224fe34853ad49ecbf69a1bd5a279ed2
guillaume arthaud <guillaume.arthaud@agence-tbd.com>
2017-02-22 14:25:20 +0100

    added message if creditentials are no gud

--- test_epi.py
+++ test_epi.py
@@ -14,12 +14,15 @@ def epiLogin():
     splittedemail = login.split('@')
 
     if splittedemail[1] == 'epitech.eu':
-        print "epitech email detected"
         auth = s.post('https://intra.epitech.eu/', data = {'format':'json','login':login,'password':password, 'remember_me':'on'})
         try:
             json_line = json.loads(auth.content)
         except ValueError:
             getInfo(s)
+        else:
+            auth = auth.json()
+            print "Can't conenct to the epitech intranet: " + auth['message']
+
 
 def getInfo(s):
     loginToGetInfos = "guillaume.arthaud@epitech.eu"
