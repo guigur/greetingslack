62682903a1d35b5024bd30b1694f9f918b2e421a
guillaume arthaud <guillaume.arthaud@agence-tbd.com>
2017-02-22 14:12:30 +0100

    added connetion to the epitech intra with the shittiest api ever made

--- test_epi.py
+++ test_epi.py
@@ -1,5 +1,30 @@
 import requests
 import json
+import Cookie
+import ConfigParser
 
-r = requests.get('https://intra.epitech.eu/', data = {'format':'json','login':'','password':''})
-print r.content
+
+def epiLogin():
+    Config = ConfigParser.ConfigParser()
+    Config.read("config.txt")
+    login = Config.get('Epitech', 'login')
+    password = Config.get('Epitech', 'password')
+    
+    s = requests.Session()
+    splittedemail = login.split('@')
+
+    if splittedemail[1] == 'epitech.eu':
+        print "epitech email detected"
+        auth = s.post('https://intra.epitech.eu/', data = {'format':'json','login':login,'password':password, 'remember_me':'on'})
+        try:
+            json_line = json.loads(auth.content)
+        except ValueError:
+            getInfo(s)
+
+def getInfo(s):
+    loginToGetInfos = "guillaume.arthaud@epitech.eu"
+    r = s.post('https://intra.epitech.eu/user/'+loginToGetInfos+'?format=json')
+    r = r.json()
+    print r['groups']
+	
+epiLogin()
